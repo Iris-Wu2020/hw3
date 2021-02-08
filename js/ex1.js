ce1419f766708d850aa5780603d5c78b91505fc3
IrisWu <xianwu_iris@126.com>
2021-02-08 20:56:08 +0800

    first commit

--- js/ex1.js
+++ js/ex1.js
@@ -2,3 +2,20 @@
 Example 1 JavaScript code
 */
 console.log("Wu's Output from Assignment 3 Exercise 1");
+
+const name = prompt("Enter your name:");
+const units = Number(prompt("Enter your number of units:"));
+
+console.log(`Hello ${name}`);
+if (units >= 0 && units <= 30) {
+    console.log("Your grade standing is Freshman");
+}
+if (units >= 31 && units <= 60) {
+    console.log("Your grade standing is Sophomore");
+}
+if (units >= 61 && units <= 90) {
+    console.log("Your grade standing is Junior");
+}
+if (units > 91) {
+    console.log("Your grade standing is Senior");
+}
