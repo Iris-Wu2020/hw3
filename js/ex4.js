8fa39b20a524016f7de34988dd1d2ed0d462cca3
IrisWu <xianwu_iris@126.com>
2021-02-09 20:06:05 +0800

    change

--- js/ex4.js
+++ js/ex4.js
@@ -3,5 +3,17 @@ Exercise 4 JavaScript code*/
 
 console.log("Wu's Output from Assignment 3 Exercise 4");
 
+let password = prompt("Please enter your password:");
+let count = 1;
+while (count < 3) {
+    if (password !== "secret") {
+        password = prompt("The password is invalid, please try again!")
+        count ++;
+    }else {
+        console.log(`You entered the correct password after ${count} attempt(s).`);
+        break;
+    }
+}
+console.log(`Your account is locked! You falied to enter the correct password ${count} times.`)
 
-
+    
--- js/ex5.js
+++ js/ex5.js
@@ -2,3 +2,8 @@
 Exercise 5 JavaScript code
 */
 console.log("Wu's Output from Assignment 3 Exercise 5");
+
+const number = Number(prompt("Enter a number:"));
+for (let i = 0; i <= 10; i++) {
+    console.log(`${number} x ${i} = ${number * i}`);
+}
