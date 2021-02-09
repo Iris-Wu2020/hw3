31e8c416847762eda83dfe1905225d5a05e7d895
IrisWu <xianwu_iris@126.com>
2021-02-09 14:36:40 +0800

    change

--- js/ex3.js
+++ js/ex3.js
@@ -2,3 +2,12 @@
 Exercise 3 JavaScript code
 */
 console.log("Wu's Output from Assignment 3 Exercise 3");
+
+let number1 = Number(prompt("Enter an integer:"));
+let number2 = Number(prompt("Enter another integer number:"));
+
+console.log(`${number1} + ${number2} = ${number1 + number2}`);
+console.log(`${number1} - ${number2} = ${number1 - number2}`);
+console.log(`${number1} * ${number2} = ${number1 * number2}`);
+console.log(`${number1} / ${number2} = ${number1 / number2}`);
+console.log(`${number1} % ${number2} = ${number1 % number2}`);
